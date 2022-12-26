622fcda60ccae11c3feb85e5754c49a2c5dc3305
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2022-12-25 23:56:25 -0500

    added code to let the user input ascii codes

--- extension.js
+++ extension.js
@@ -6,7 +6,24 @@ const vscode = require('vscode');
 function activate(context) {
 
 	let disposable = vscode.commands.registerCommand('ascii-commander.getAsciiSymbol', function () {
-		vscode.window.showInformationMessage('Hello World from Ascii Commander!');
+		vscode.window.showInputBox({
+			prompt:"Type ascii code", 
+			title:"Ascii Commander",
+			validateInput: text => {
+				return (/^[0-9]*$/g.test(text) && (parseInt(text)>=0 && parseInt(text)<=255)) ? null : "Add valid ascii code" 
+			}
+		}).then(text=>{
+			if(text === undefined) return false
+			const editor = vscode.window.activeTextEditor
+			if(editor === undefined) return false
+			const cursorPosition = vscode.window.activeTextEditor.selection.active;
+			if(cursorPosition === undefined) return false
+			const asciiSymbol = String.fromCharCode(parseInt(text))
+			editor.edit(editBuilder=>{
+				editBuilder.insert(cursorPosition, asciiSymbol)
+			})
+
+		})
 	});
 
 	context.subscriptions.push(disposable);
